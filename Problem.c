8195809ada18b8ff8bef998a67d59edfaf95e90c
Kamlesh <ky2673689@gmail.com>
2025-02-17 22:06:04 +0530

    first commit

---
+++ Problem.c
@@ -0,0 +1,16 @@
+// Swaping of two Numbers
+#include <stdio.h>
+
+int main() {
+
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+    printf("a = %d\n",a);
+    printf("b = %d",b);
+    
+}   
